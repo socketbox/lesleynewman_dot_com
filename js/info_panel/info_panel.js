b32e4524385f198c10a016ba43daaa56e780f519
Casey Boettcher <cboettcher@gmail.com>
2012-07-10 13:52:41 -0500

    Initial commit and push. Well overdue.

---
+++ js/info_panel/info_panel.js
@@ -0,0 +1,136 @@
+/**
+ * Created with PyCharm.
+ * User: chb
+ * Date: 7/4/12
+ * Time: 8:47 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var overlays = {};
+//var $closeCircle = null;
+
+//create the close icon
+var $closeCircle = $("<img/>");
+$closeCircle.attr("class", "close_svg");
+$closeCircle.attr("alt", "click here to close");
+$closeCircle.attr("src", "/images/svg/close_circle.svg");
+$closeCircle.attr("width", "43px");
+$closeCircle.attr("height", "43px");
+
+$(document).ready(
+    function()
+    {
+        //info_panel_data is gotten from the js file of the same name
+        createDivOverlays(info_panel_data);
+
+        /*on ready, put click handlers on icons and label svgs;
+          pass in fully-formed and category-specific div as event.data
+        */
+        $(document).on("click", "img[alt='biographical info']", overlays.bio, onClickInfoPanel);
+        $(document).on("click", "img[alt='work experience']", overlays.work, onClickInfoPanel);
+        $(document).on("click", "img[alt='academic accomplishments']", overlays.edu, onClickInfoPanel);
+        $(document).on("click", "img[alt='contact form']", overlays.form, onClickInfoPanel);
+        $("img[alt='click here to close']").live("click", removeInfoPanel);
+
+    }
+);
+
+function createDivOverlays(info_panel_data)
+{
+    var $newDiv = null;
+    var factArray = null;
+    //returns ["bio","work","edu"]
+    var keys = Object.keys(info_panel_data);
+
+    var keyName = null;
+    for (var k in keys)
+    {
+        //get string for index
+        keyName = keys[k];
+        $newDiv = $("<div/>");
+        $newDiv.attr("id", keyName + "_info_panel");
+        $newDiv.attr("class", "info_panel");
+        factArray = Object.getOwnPropertyDescriptor(info_panel_data, keyName).value;
+        var fact = null;
+        for (var f in factArray)
+        {
+           fact = factArray[f];
+           $newDiv.html(function(index, oldHtml){
+               var htmlContent = oldHtml + "<p>" + fact + "</p>";
+               return htmlContent;
+           });
+        }
+        //put the close icon in the new div
+        $newDiv.prepend($closeCircle.clone());
+        //put the new div in the wrapping map/object
+        Object.defineProperty(overlays, keyName, { value: $newDiv, writeable: true});
+    }
+
+    //create the contact form and put it in the div map
+    $contactForm = constructContactForm();
+
+}
+
+function constructContactForm()
+{
+
+    var $subjectField = $('<input type="text" maxlength="128"/>');
+    var $bodyField = $('<input type="textarea" maxlength="1024"/>');
+    var $emailField = null;
+
+    /*if( Modernizr.input.required )
+    {
+        $subjectField.attr("required", "required");
+
+        if( Modernizr.inputtypes.email )
+        {
+           $emailField = $("<email/>");
+        }
+    }
+    else
+    {
+        //put jquery foo here
+    }
+
+    */
+
+    var $newDiv = $("<div/>");
+    $newDiv.attr("id", "contact_form");
+    $newDiv.attr("class", "info_panel");
+
+    //now build the form
+    var $jqForm = $("<form />");
+
+    var $subjectField = $("<input/>");
+    $subjectField.attr("type", "text");
+    $subjectField.attr("placeholder", "subject");
+    $jqForm.append($emailField);
+    $jqForm.append($subjectField);
+
+
+    $newDiv.prepend($closeCircle.clone());
+    $newDiv.append($jqForm);
+
+    var keyName = "form";
+    Object.defineProperty(overlays, keyName, {value: $newDiv, writeable: true});
+}
+
+function removeInfoPanel(event)
+{
+    var $panel = $(".info_panel");
+    $panel.remove();
+    var $page = $("#page");
+    $page.show();
+}
+
+function onClickInfoPanel( event )
+{
+    var $infoPanel = event.data;
+    var $page = $("div#page");
+
+    $page.hide(0, function(){
+        $("#wrapper").prepend( $infoPanel );
+        $infoPanel.fadeIn(); }
+    );
+};
+
---
+++ js/info_panel/info_panel_data.js
@@ -0,0 +1,36 @@
+/**
+ * Created with PyCharm.
+ * User: chb
+ * Date: 7/8/12
+ * Time: 11:39 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var bio_info = [
+    "Lived in Virginia; Portland, Oregon; New York City, New York; and Tucson, Arizona in the United States",
+    "Has also resdided in Lismore, Australia; Dajabon, Dominican Republic; Oaxaca, Mexico; and Danang, Vietnam",
+    "Visited Vancouver, Canada; North Island, New Zealand; San Salvador, El Salvador; Bogota and Medellín, Colombia",
+    "Enjoys rock climbing, hiking, camping, and, of course, traveling",
+    "Likes to watch good movies (always with popcorn), finish crossword puzzles, and win at Scrabble"];
+
+var work_info = [
+    "Taught English for community programs in New York City, New York; Tucson and Nogales, Arizona; and Sonora, Mexico",
+    "English instructor at the university level: CESL and Pima Community College Intensive English Programs, Tucson, Arizona",
+    "Trained teachers in the Dominican Republic and online for Chungnam University, Korea",
+    "Presented on developing writing and critical thinking skills in language classrooms",
+    "Experience in developing curricula, training and managing volunteers, planning projects, recruiting, and translation"];
+
+var edu_info = [
+    "Bachelor's degree from James Madison University in Harrisonburg, Virginia",
+    "Courses in Outdoor Education and Women's Literature at Southern Cross University, Lismore, Australia",
+    "Earned Master's degree from the University of Arizona, in Tucson, Arizona",
+    "Awarded Fulbright Garcia-Robles grant to study and teach in Oaxaca, Mexico",
+    "Accepted English Language Fellow position with Georgetown, University in Da Nang, Vietnam"];
+    /*"Received my Bachelors degree from James Madison University in Harrisonburg, Virginia",
+    "While in undergraduate studies, played Club Field Hockey, participated in several Alternative Spring Break trips, studied abroad, and worked part-time",
+    "Studied Outdoor Education and Women's Literature at Southern Cross University, Lismore, Australia",
+    "Received Master's degree from the University of Arizona, in Tucson, Arizona",
+    "During graduate studies, played intramural soccer, participated as a Peace Corps Fellow fulfilling 1,000 hour community service requirement, represented my cohort in the English Language and Linguistics Student Association as vice-president, then president, and worked part-time",
+    "Received Fulbright Garcia-Robles grant to study and teach in Oaxaca, Mexico"];
+    */
+var info_panel_data = {"bio": bio_info, "work": work_info, "edu": edu_info };
